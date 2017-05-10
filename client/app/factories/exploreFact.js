cc2639b53f94fdc2b38b3c05c7a2c940999ace3b
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-05-10 11:09:20 -0500

    got explore page working displaying results

--- client/app/factories/exploreFact.js
+++ client/app/factories/exploreFact.js
@@ -5,14 +5,14 @@ app.factory('topFactory', ['$http', function ($http) {
     const topFactory = {}
 
     const redditAPI = 'http://reddit.com/r/popular/.json?'
-    const get5P = function() {
-        return $http.get(`${redditAPI}limit=5`).then(function(top5results) {
-            console.log("top5results", top5results)
-            return top5results.data.data.children;
+    const get6P = function() {
+        return $http.get(`${redditAPI}limit=6`).then(function(top6results) {
+            // console.log("top6results", top6results)
+            return top6results.data.data.children;
         })
     }
 
-    topFactory.get5P = get5P;
+    topFactory.get6P = get6P;
     return topFactory;
 
 }]);
