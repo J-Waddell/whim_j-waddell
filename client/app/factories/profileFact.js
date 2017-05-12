da1cce403710d1a83d584812a03a98bc80d7c685
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-05-12 11:37:36 -0500

    working on getting saved data to appear on profile

--- client/app/factories/profileFact.js
+++ client/app/factories/profileFact.js
@@ -5,11 +5,11 @@ app.factory('profileFactory', function($http) {
     const profileFactory = {}
     const postedURL = './addUserPost'
 
-    // const getPost = function() {
-    //     return $http.get(``).then(function(favorited) {
-    //         return favorited.data.data.children
-    //     })
-    // }
+    const getPost = function() {
+        return $http.get(`./addUserPost`).then(function(favorited) {
+            return favorited.data.data.children
+        })
+    }
 
     // profileFactory.userLike = userLike;
     // profileFactory.getPost = getPost;
